f2adbef58bfdb8227548f91eea8b4fb1cbeba92a
ozlocke <oz.locke@gmail.com>
2023-03-06 10:37:25 +0000

    Initial set up of data and sitemap

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# transinformative
+# Transinformative
+
 A simple Q &amp; A format collection of facts on trans people, trans science, and trans life
+
+## Site structure
+
+```mermaid
+
+mindmap
+  root((Trans people...))
+    ...scientifically.
+        Chromosomes
+        Sex
+        Gender
+        Hormones
+    ...as criminals.
+        Rape
+        Assault
+        Predation
+        Pedophelia
+    ...as victims.
+        Rape
+        Assault
+        Predation
+        Harassment
+        Murder
+        Genocide
+    ...in politics.
+        Laws
+        Politicians
+    ...in the media.
+        Social media
+        Misinformation
+        Drag
+        News
+
+```
---
+++ csv_to_json.py
@@ -0,0 +1,27 @@
+import csv 
+import json
+
+#code taken from here: https://stackoverflow.com/questions/56113592/convert-csv-to-json-file-in-python
+
+def csv_to_json(csvFilePath, jsonFilePath):
+    jsonArray = []
+      
+    #read csv file
+    with open(csvFilePath, encoding='utf-8') as csvf: 
+        #load csv file data using csv library's dictionary reader
+        csvReader = csv.DictReader(csvf) 
+
+        #convert each csv row into python dict
+        for row in csvReader: 
+            #add this python dict to json array
+            jsonArray.append(row)
+  
+    #convert python jsonArray to JSON String and write to file
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf: 
+        jsonString = json.dumps(jsonArray, indent=4)
+        jsonf.write(jsonString)
+          
+csvFilePath = r'q_and_a_data.csv'
+jsonFilePath = r'q_and_a_data.json'
+
+csv_to_json(csvFilePath, jsonFilePath)
